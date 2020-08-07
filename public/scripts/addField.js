be79b54b1af3e0582e8b942b4856e6b48ab6a6e7
deckerleonardo <deckerleonardo@gmail.com>
2020-08-06 22:56:34 -0300

    quarto dia

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,6 +1,5 @@
-/* Procurar o botão na DOM */ 
+/* Procurar o botão */
 const btnAddField = document.querySelector("#add-time");
-/* Procurar o fieldset schedule items */
 const scheduleItems = document.querySelector("#schedule-items");
 
 /* Adiciona um evento quando clicar no botão */
@@ -10,13 +9,14 @@ btnAddField.addEventListener("click", () => { addField(); });
 function addField() {
     /* Copiar o schedule Item */
     const newScheduleItem = document.querySelector('.schedule-item').cloneNode(true);
-    /* Limpar os campos */
+    /* pegar os campos */
     const scheduleItem = newScheduleItem.querySelectorAll('input');
     /* Percorrer cada campo do schedule item */
     scheduleItem.forEach((field) => {
         /* Limpa o campo */
         field.value = "";
     });
+    
     /* Adicionar ao fieldset o elemento clonado */
     scheduleItems.appendChild(newScheduleItem);
 }  
